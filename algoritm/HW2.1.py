c71868e546481b278de9fd0e8867f407f8258229
oleg31947 <oleg31947@gmail.com>
2019-12-25 10:12:35 -0800

    HW2.1 - problem

--- algoritm/HW2.1.py
+++ algoritm/HW2.1.py
@@ -9,11 +9,25 @@ def main(my_greeting):
 
 # remove spaces
 my_gift = "  Wellcom to     Portland    "
-new_my_gift = "".join(my_gift).split()
+new_my_gift = ''.join(my_gift).split()
+concatenate = ''.join([str(elem) for elem in new_my_gift])
 
+
+#def unite_gift(*args):
+   # new_string = []
+    #for arg in args:
+        #new_string += arg
+    #return new_string
+
+def unite_gift(s):
+    new_string = ""
+    for arg in s:
+        new_string += arg
+    return new_string
 
 
 if __name__ == "__main__":
     print(main(my_greeting="Oleg_Morozov is the best men"))
-
     print(new_my_gift)
+    print(unite_gift(new_my_gift))
+    print(concatenate)
